e37bd5e26c3b0bf85ef6a6eeb3529f976c909caf
Michael Hendricks <michael@ndrix.org>
2017-01-25 14:51:15 -0500

    Move event handlers to separate functions
    Some of these functions will grow to be quite large.  Having them
    stand on their own makes them easier to work with.

--- component.go
+++ component.go
@@ -22,32 +22,40 @@ func Main(config Config) {
 	}
 
 	// Uppercase Echo Component
-	c.MessageHandler = func(c *xco.Component, m *xco.Message) error {
-		log.Printf("Message: %+v", m)
-		if m.Body == "" {
-			log.Printf("  ignoring message with empty body")
-			return nil
-		}
-		resp := m.Response()
-		resp.Body = strings.ToUpper(m.Body)
-		log.Printf("Responding: %+v", resp)
-		return errors.Wrap(c.Send(resp), "sending response")
-	}
-	c.PresenceHandler = func(c *xco.Component, p *xco.Presence) error {
-		log.Printf("Presence: %+v", p)
-		return nil
-	}
-	c.IqHandler = func(c *xco.Component, iq *xco.Iq) error {
-		log.Printf("Iq: %+v", iq)
-		return nil
-	}
-	c.UnknownHandler = func(c *xco.Component, x *xml.StartElement) error {
-		log.Printf("Unknown: %+v", x)
-		return nil
-	}
+	c.MessageHandler = onMessage
+	c.PresenceHandler = onPresence
+	c.IqHandler = onIq
+	c.UnknownHandler = onUnknown
 
 	err = c.Run()
 	if err != nil {
 		log.Printf("ERROR: Run: %s", err)
 	}
+}
+
+func onMessage(c *xco.Component, m *xco.Message) error {
+	log.Printf("Message: %+v", m)
+	if m.Body == "" {
+		log.Printf("  ignoring message with empty body")
+		return nil
+	}
+	resp := m.Response()
+	resp.Body = strings.ToUpper(m.Body)
+	log.Printf("Responding: %+v", resp)
+	return errors.Wrap(c.Send(resp), "sending response")
+}
+
+func onPresence(c *xco.Component, p *xco.Presence) error {
+	log.Printf("Presence: %+v", p)
+	return nil
+}
+
+func onIq(c *xco.Component, iq *xco.Iq) error {
+	log.Printf("Iq: %+v", iq)
+	return nil
+}
+
+func onUnknown(c *xco.Component, x *xml.StartElement) error {
+	log.Printf("Unknown: %+v", x)
+	return nil
 }
